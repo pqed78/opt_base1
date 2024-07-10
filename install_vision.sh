c2610f39cd6c5b3411ae45b871ae002fe9acb577
Taek Yong Hwang <taekyong.hwang@gmail.com>
2024-07-10 20:30:42 +0900

    install.sh update

--- install_vision.sh
+++ install_vision.sh
@@ -3,7 +3,8 @@
 
 #/env/bin/python -m pip install --extra-index-url https://artifacts.luxonis.com/artifactory/luxonis-python-snapshot-local/ depthai
 sudo python -mpip install depthai
-/env/bin/python -m pip install ultralytics[export]
+sudo python -mpip install ultralytics[export]
+# /env/bin/python -m pip install ultralytics[export]
 /env/bin/python -m pip uninstall torch opencv-python -y
 /env/bin/python -m pip install opencv-python==4.9.0.80
 
